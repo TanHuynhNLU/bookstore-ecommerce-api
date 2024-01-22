1818cc05ce0ea02ad1dc373bb867d0e008d881ae
Huynh Ngoc Tan <18130210@st.hcmuaf.edu.vn>
2024-01-22 09:38:44 +0700

    Initial commit

---
+++ src/main/java/com/example/bookstoreecommerceapi/BookstoreEcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookstoreecommerceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreEcommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreEcommerceApiApplication.class, args);
+	}
+
+}
